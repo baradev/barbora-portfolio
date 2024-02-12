3b56f4bb8cc2abdf4fc12d469baefd4fae4d5dd5
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-12 16:25:35 +1300

    add new projects

--- src/data/user.js
+++ src/data/user.js
@@ -9,7 +9,7 @@ const INFO = {
 	socials: {
 		github: "https://github.com/baradev",
 		linkedin: "https://www.linkedin.com/in/barboraklu",
-		instagram: "https://www.instagram.com/npm_isntall_unicorns",
+		instagram: "https://www.instagram.com/npm_install_unicorns",
 	},
 
 	homepage: {
@@ -19,7 +19,7 @@ const INFO = {
 	},
 
 	about: {
-		title: "I’m Barbora. I live in Auckland, New Zealand.",
+		title: "I’m Barbora. I live in Auckland.",
 		description:
 			"During the last year, I've worked on a range of projects, and I'm proud of the progress achieved. A number of these projects are open-source. If any of these projects interest you, please feel free to take a look at the code and suggest any improvements or enhancements you think could be made. Collaborating with others is a valuable way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
@@ -32,21 +32,22 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "Habit Tracker",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+				"Habit Tracker is a Next.js app that allows its users to track their habits. Skills applied include Tailwind CSS, HTML, TypeScript, React, Next.js, and deployment on Vercel.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://my-habit-tracker-orcin.vercel.app/",
 		},
 
 		{
-			title: "Project 2",
+			title: "Blog App",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+				"This app allows its users to sign in with GitHub account and publish blog posts. Skills applied include HTML, CSS, Next.js, Prisma, and Vercel.",
+			logo: "https://www.cdnlogo.com/logos/n/80/next-js.svg",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://barbora-klu-blogr-nextjs-prisma.vercel.app/",
 		},
 
 		{
